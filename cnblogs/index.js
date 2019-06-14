0058de3dc4169d66bacd6fc0f91e5559bfdc5e8b
snowdreams1006 <snowdreams1006@163.com>
2019-06-14 12:45:51 +0800

    add cnblogs

--- cnblogs/index.js
+++ cnblogs/index.js
@@ -159,66 +159,24 @@ function parseCurrent($) {
         // 标题以及链接
         var titile = $(article).find("td:nth-child(1) a").text().trim();
         var href = $(article).find("td:nth-child(1) a").attr("href");
-
-        console.log("titile", titile);
-        console.log("href", href);
-
         // 评论数和阅读数
-        var commentCount = $(article).find("td:nth-child(3)").text().trim();
-        
-        console.log("commentCount", commentCount);
+        var commentCount = ($(article).find("td:nth-child(3)").text().trim()) * 1;
+        var readCount = ($(article).find("td:nth-child(4)").text().trim()) * 1;
 
-        var readCount = $(article).find("td:nth-child(4)").text().trim();
-
-        console.log("readCount", readCount);
+        // 文章汇总数据
+        result.atricles.push({
+            "titile": titile,
+            "href": href
+        });
+        result.commentCount += commentCount;
+        result.readCount += readCount;
 
         // 总体概况
         console.log(`一共解析出${atricles.length}篇文章,正在解析第${i+1}篇,标题: ${titile} 评论数: ${commentCount} 阅读数: ${readCount}`);
     }
 
-
-
-
-
-
-    // 解析当前页
-    var atricles = $("#articlesList div.article-item");
-    for (var i = 0; i < atricles.length; i++) {
-        var article = atricles[i];
-
-        var header = $(article).find("h3.item-title");
-        var body = $(article).find("div.item-txt");
-        var footer = $(article).find("div.item-btm");
-
-        // 标题以及链接
-        var titile = $(header).find("a.title-detail").text().trim();
-        var href = $(header).find("a.title-detail").attr("href");
-        // 内容概要
-        var content = $(body).find("p.item-bd").text()
-        // 阅读量,推荐量以及评论量
-        var readCount = $(footer).find(".right-info > div:nth-child(1) > em").text().trim();
-        readCount = (readCount.substr(0, readCount.lastIndexOf("浏览"))) * 1
-        var recommendCount = $(footer).find(".right-info > div:nth-child(2) > em").text().trim();
-        recommendCount = (recommendCount.substr(0, recommendCount.lastIndexOf("推荐"))) * 1;
-        var commentCount = $(footer).find(".right-info > div:nth-child(3) > em").text().trim();
-        commentCount = (commentCount.substr(0, commentCount.lastIndexOf("评论"))) * 1;
-
-        // 文章汇总数据
-        result.atricles.push({
-            "titile": titile,
-            "href": href,
-            "content": content
-        });
-        result.readCount += readCount;
-        result.recommendCount += recommendCount;
-        result.commentCount += commentCount;
-
-        // 当前页正在解析中
-        console.log(`当前页面解析中,一共${atricles.length}篇文章,正在解析第${i+1}篇,标题: ${titile} 阅读量: ${readCount} 推荐量: ${recommendCount} 评论数: ${commentCount}`);
-    }
-
     // 当前页解析完毕
     console.log();
-    console.log(`当前页面解析完毕,一共${result.atricles.length}篇文章, 阅读量: ${result.readCount} 推荐量: ${result.recommendCount} 评论数: ${result.commentCount}`);
+    console.log(`当前页面解析完毕,一共${result.atricles.length}篇文章, 评论数: ${result.commentCount} 阅读数: ${result.readCount}`);
     console.log();
 }
