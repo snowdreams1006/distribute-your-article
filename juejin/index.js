83f22b718c29bc7a9b6fdab2611fa46791707b3c
snowdreams1006 <snowdreams1006@163.com>
2019-06-14 14:45:29 +0800

    parseCurrent

--- juejin/index.js
+++ juejin/index.js
@@ -50,51 +50,63 @@ function readCookie() {
 async function indexWithCookie(requestConfig) {
     try {
 
-        // 初次访问解析出分页总数,并不计数
-        var body = await syncRequest(requestConfig);
+        // 解析首页数据
+        await parseIndex();
 
-        // 数据保存到本地
-        fs.writeFileSync(`./data/${now.format("YYYY-MM-DD")}.html`, body);
-
-        //默认规则分页查询
-        requestConfig.url = "https://timeline-merger-ms.juejin.im/v1/get_entry_by_self";
-        requestConfig.qs = {
-            "src": "web",
-            "uid": "582d5cb667f356006331e586",
-            "device_id": "1560005329014",
-            "token": "eyJhY2Nlc3NfdG9rZW4iOiJENVp4b2l6ajlUeEdmeGpxIiwicmVmcmVzaF90b2tlbiI6IlhZYWNvTEk5WUVxNHJpcUIiLCJ0b2tlbl90eXBlIjoibWFjIiwiZXhwaXJlX2luIjoyNTkyMDAwfQ%3D%3D",
-            "targetUid": "582d5cb667f356006331e586",
-            "type": "post",
-            "before": "",
-            "limit": "20",
-            "order": "createdAt"
-        };
-
-        body = await syncRequest(requestConfig);
-
-        // 数据保存到本地
-        fs.writeFileSync(`./data/${now.format("YYYY-MM-DD")}-${requestConfig.qs.before}.json`, body);
-
-        parseCurrent(body);
-
-        // for (var i = 1; i <= total; i++) {
-        //     requestConfig.qs = {
-        //         "page": i
-        //     };
-
-        //     var body = await syncRequest(requestConfig);
-
-        //     // 数据保存到本地
-        //     fs.writeFileSync(`./data/${now.format("YYYY-MM-DD")}.html`, body);
-
-        //     parseCurrent(cheerio.load(body));
-        // }
-
-        // // 数据保存到本地
-        // fs.writeFileSync(`./data/${now.format("YYYY-MM-DD")}.json`, JSON.stringify(result));
+        // 请求分页数据
+        await requestPagenationData("");
 
     } catch (error) {
         console.error("error", error);
+    }
+}
+
+/**
+ * 解析首页
+ */
+async function parseIndex() {
+    // 初次访问解析出分页总数,并不计数
+    var body = await syncRequest(requestConfig);
+
+    // 判断是否登录
+    isLogin(cheerio.load(body));
+
+    // 数据保存到本地
+    fs.writeFileSync(`./data/${now.format("YYYY-MM-DD")}.html`, body);
+}
+
+/**
+ * 请求数据
+ */
+async function requestPagenationData(before) {
+    console.log("requestPagenationData:before", before);
+
+    //默认规则分页查询
+    requestConfig.url = "https://timeline-merger-ms.juejin.im/v1/get_entry_by_self";
+    requestConfig.qs = {
+        "src": "web",
+        "uid": "582d5cb667f356006331e586",
+        "device_id": "1560005329014",
+        "token": "eyJhY2Nlc3NfdG9rZW4iOiJENVp4b2l6ajlUeEdmeGpxIiwicmVmcmVzaF90b2tlbiI6IlhZYWNvTEk5WUVxNHJpcUIiLCJ0b2tlbl90eXBlIjoibWFjIiwiZXhwaXJlX2luIjoyNTkyMDAwfQ%3D%3D",
+        "targetUid": "582d5cb667f356006331e586",
+        "type": "post",
+        "before": before,
+        "limit": "20",
+        "order": "createdAt"
+    };
+
+    var body = await syncRequest(requestConfig);
+
+    // 数据保存到本地
+    fs.writeFileSync(`./data/${now.format("YYYY-MM-DD")}-${requestConfig.qs.before}.json`, body);
+
+    // 解析当前分页数据
+    before = parseCurrent(body);
+    console.log("parseCurrent:before", before);
+
+    // 递归请求分页数据
+    if (before) {
+        requestPagenationData(before);
     }
 }
 
@@ -128,7 +140,7 @@ function isLogin($) {
 
         return true;
     } else {
-        console.log("尚未登录: " + body);
+        console.log("尚未登录");
 
         return false;
     }
@@ -138,42 +150,45 @@ function isLogin($) {
  *  解析首页
  * @param {html} body 
  */
-function parseCurrent($) {
+function parseCurrent(body) {
     // 解析文章基本信息
-    var atricles = $("#list-container li");
-    for (var i = 0; i < atricles.length; i++) {
-        var article = atricles[i];
+    body = JSON.parse(body);
+    if (body.s == 1 && body.d.total > 0) {
+        var atricles = body.d.entrylist;
+        for (var i = 0; i < atricles.length; i++) {
+            var article = atricles[i];
 
-        var header = $(article).find("a.title");
-        var body = $(article).find("p.abstract");
-        var footer = $(article).find("div.meta");
+            // 标题以及链接
+            var titile = article.titile;
+            var href = article.originalUrl;
+            // 内容概要
+            var content = article.content;
+            // 阅读量,评论量以及喜欢
+            var readCount = article.viewsCount;
+            var commentCount = article.commentsCount;
+            var recommendCount = article.collectionCount;
 
-        // 标题以及链接
-        var titile = $(header).text().trim();
-        var href = $(header).attr("href");
-        // 内容概要
-        var content = $(body).text();
-        // 阅读量,评论量以及喜欢
-        var readCount = ($(footer).find(".ic-list-read").parent().text().trim()) * 1;
-        var commentCount = ($(footer).find(".ic-list-comments").parent().text().trim()) * 1;
-        var recommendCount = ($(footer).find(".ic-list-like").parent().text().trim()) * 1;
+            // 文章汇总数据
+            result.atricles.push({
+                "titile": titile,
+                "href": href,
+                "content": content
+            });
+            result.readCount += readCount;
+            result.commentCount += commentCount;
+            result.recommendCount += recommendCount;
 
-        // 文章汇总数据
-        result.atricles.push({
-            "titile": titile,
-            "href": href,
-            "content": content
-        });
-        result.readCount += readCount;
-        result.commentCount += commentCount;
-        result.recommendCount += recommendCount;
+            // 当前页正在解析中
+            console.log(`当前页面解析中,一共${atricles.length}篇文章,正在解析第${i+1}篇,标题: ${titile} 点赞数: ${recommendCount} 评论数: ${commentCount} 阅读数: ${readCount}`);
+        }
 
-        // 当前页正在解析中
-        console.log(`当前页面解析中,一共${atricles.length}篇文章,正在解析第${i+1}篇,标题: ${titile} 阅读量: ${readCount} 评论数: ${commentCount} 喜欢数: ${recommendCount}`);
+        return atricles[atricles.length - 1].createdAt;
     }
 
     // 当前页解析完毕
     console.log();
-    console.log(`当前页面解析完毕,一共${result.atricles.length}篇文章, 阅读量: ${result.readCount} 评论数: ${result.commentCount} 喜欢数: ${result.recommendCount}`);
+    console.log(`当前页面解析完毕,一共${result.atricles.length}篇文章, 点赞数: ${recommendCount} 评论数: ${commentCount} 阅读数: ${readCount}`);
     console.log();
+
+    return;
 }
