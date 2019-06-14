d073db2643258bbcf03a096199eeb276fac7e317
snowdreams1006 <snowdreams1006@163.com>
2019-06-14 13:14:33 +0800

    parseIndex

--- juejin/index.js
+++ juejin/index.js
@@ -56,42 +56,4 @@ function parseIndex(body) {
     } else {
         console.log("尚未登录: " + body);
     }
-
-    // // 解析文章基本信息
-    // var atricles = $("#list-container li");
-    // for (var i = 0; i < atricles.length; i++) {
-    //     var article = atricles[i];
-
-    //     var header = $(article).find("a.title");
-    //     var body = $(article).find("p.abstract");
-    //     var footer = $(article).find("div.meta");
-
-    //     // 标题以及链接
-    //     var titile = $(header).text().trim();
-    //     var href = $(header).attr("href");
-
-    //     console.log("titile", titile);
-    //     console.log("href", href);
-
-    //     // 内容概要
-    //     var content = $(body).text();
-
-    //     console.log("content", content);
-
-    //     // 阅读量,评论量以及喜欢
-    //     var readCount = $(footer).find(".ic-list-read").parent().text().trim();
-
-    //     console.log("readCount", readCount);
-
-    //     var commentCount = $(footer).find(".ic-list-comments").parent().text().trim();
-        
-    //     console.log("commentCount", commentCount);
-
-    //     var recommendCount = $(footer).find(".ic-list-like").parent().text().trim();
-        
-    //     console.log("recommendCount", recommendCount);
-
-    //     // 总体概况
-    //     console.log(`一共解析出${atricles.length}篇文章,正在解析第${i+1}篇,标题: ${titile} 阅读量: ${readCount} 评论数: ${commentCount} 喜欢数: ${recommendCount}`);
-    // }
 }
