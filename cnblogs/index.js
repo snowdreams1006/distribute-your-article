1bc5d1d045b8b337cdd593789c03b5cc2d54aa3a
snowdreams1006 <snowdreams1006@163.com>
2019-06-14 12:39:12 +0800

    remove parseIndex2

--- cnblogs/index.js
+++ cnblogs/index.js
@@ -151,6 +151,36 @@ function parsePagenation($) {
  * @param {html} body 
  */
 function parseCurrent($) {
+    // 解析文章基本信息
+    var atricles = $("#post_list tr");
+    for (var i = 1; i < atricles.length; i++) {
+        var article = atricles[i];
+
+        // 标题以及链接
+        var titile = $(article).find("td:nth-child(1) a").text().trim();
+        var href = $(article).find("td:nth-child(1) a").attr("href");
+
+        console.log("titile", titile);
+        console.log("href", href);
+
+        // 评论数和阅读数
+        var commentCount = $(article).find("td:nth-child(3)").text().trim();
+        
+        console.log("commentCount", commentCount);
+
+        var readCount = $(article).find("td:nth-child(4)").text().trim();
+
+        console.log("readCount", readCount);
+
+        // 总体概况
+        console.log(`一共解析出${atricles.length}篇文章,正在解析第${i+1}篇,标题: ${titile} 评论数: ${commentCount} 阅读数: ${readCount}`);
+    }
+
+
+
+
+
+
     // 解析当前页
     var atricles = $("#articlesList div.article-item");
     for (var i = 0; i < atricles.length; i++) {
@@ -191,47 +221,4 @@ function parseCurrent($) {
     console.log();
     console.log(`当前页面解析完毕,一共${result.atricles.length}篇文章, 阅读量: ${result.readCount} 推荐量: ${result.recommendCount} 评论数: ${result.commentCount}`);
     console.log();
-}
-
-/**
- *  解析首页
- * @param {html} body 
- */
-function parseIndex2(body) {
-    var $ = cheerio.load(body);
-
-    // 已经登录会出现后台管理页面,尚未登录则会自动跳转登录页面
-    var userBlog = $("#blog_title > a");
-
-    var loginFlag = userBlog && userBlog.attr("href");
-    if (loginFlag) {
-        console.log("已经登录: " + userBlog.attr("href"));
-    } else {
-        console.log("尚未登录: " + body);
-    }
-
-    // 解析文章基本信息
-    var atricles = $("#post_list tr");
-    for (var i = 1; i < atricles.length; i++) {
-        var article = atricles[i];
-
-        // 标题以及链接
-        var titile = $(article).find("td:nth-child(1) a").text().trim();
-        var href = $(article).find("td:nth-child(1) a").attr("href");
-
-        console.log("titile", titile);
-        console.log("href", href);
-
-        // 评论数和阅读数
-        var commentCount = $(article).find("td:nth-child(3)").text().trim();
-        
-        console.log("commentCount", commentCount);
-
-        var readCount = $(article).find("td:nth-child(4)").text().trim();
-
-        console.log("readCount", readCount);
-
-        // 总体概况
-        console.log(`一共解析出${atricles.length}篇文章,正在解析第${i+1}篇,标题: ${titile} 评论数: ${commentCount} 阅读数: ${readCount}`);
-    }
 }
