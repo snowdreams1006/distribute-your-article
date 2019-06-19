bf7a0eae68b8bc7004578585c01f0bae3f072578
snowdreams1006 <snowdreams1006@163.com>
2019-06-19 14:33:48 +0800

    index

--- cloud-tencent/index.js
+++ cloud-tencent/index.js
@@ -13,6 +13,7 @@ var cookie = readCookie('cloud-tencent');
 // 请求参数
 var requestConfig = {
     url: 'https://cloud.tencent.com/developer/user/2952369/activities',
+    method: 'GET',
     headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
         'Cookie': cookie
@@ -29,7 +30,7 @@ var result = {
 };
 
 // 模拟登录直接访问首页
-indexWithCookie(requestConfig);
+indexWithCookie();
 
 /**
  * 读取 cookie(自定义 cookie)
@@ -41,40 +42,42 @@ function readCookie(cookieKey) {
 }
 
 /**
- * 同步访问首页(自定义 cookie)
+ * 同步访问首页
+ * 
+ * 渲染流程:初次整体渲染,随后ajax异步加载
  */
-async function indexWithCookie(requestConfig) {
+async function indexWithCookie() {
     try {
         // 解析首页数据
-        await parseIndex();
+        await parseIndexHtml();
 
-        // 解析出分页总数,依次遍历访问累加
-        var total = await parsePagenation();
-        for (var i = 1; i <= total; i++) {
-            //默认规则分页查询
-            requestConfig.url = "https://cloud.tencent.com/developer/services/ajax/user-center";
-            requestConfig.method = "POST";
-            requestConfig.json = true;
-            requestConfig.qs = {
-                "action": "GetUserActivities",
-                "uin": "100005824907",
-                "csrfCode": "502094663"
-            };
-            requestConfig.body = {
-                "action": "GetUserActivities",
-                "payload": {
-                    "uid": 2952369,
-                    "pageNumber": i
-                }
-            };
+        // // 解析出分页总数,依次遍历访问累加
+        // var total = await parsePagenation();
+        // for (var i = 1; i <= total; i++) {
+        //     //默认规则分页查询
+        //     requestConfig.url = "https://cloud.tencent.com/developer/services/ajax/user-center";
+        //     requestConfig.method = "POST";
+        //     requestConfig.json = true;
+        //     requestConfig.qs = {
+        //         "action": "GetUserActivities",
+        //         "uin": "100005824907",
+        //         "csrfCode": "502094663"
+        //     };
+        //     requestConfig.body = {
+        //         "action": "GetUserActivities",
+        //         "payload": {
+        //             "uid": 2952369,
+        //             "pageNumber": i
+        //         }
+        //     };
 
-            var body = await syncRequest(requestConfig);
+        //     var body = await syncRequest(requestConfig);
 
-            // 数据保存到本地
-            fs.writeFileSync(`./data/${now.format("YYYY-MM-DD")}[${i}].json`, JSON.stringify(body));
+        //     // 数据保存到本地
+        //     fs.writeFileSync(`./data/${now.format("YYYY-MM-DD")}[${i}].json`, JSON.stringify(body));
 
-            parseCurrent(body);
-        }
+        //     parseCurrent(body);
+        // }
 
         // 数据保存到本地
         fs.writeFileSync(`./data/${now.format("YYYY-MM-DD")}.json`, JSON.stringify(result));
@@ -87,7 +90,7 @@ async function indexWithCookie(requestConfig) {
 /**
  * 解析首页
  */
-async function parseIndex() {
+async function parseIndexHtml() {
     // 初次访问解析出分页总数,并不计数
     var body = await syncRequest(requestConfig);
 
@@ -96,6 +99,43 @@ async function parseIndex() {
 
     // 数据保存到本地
     fs.writeFileSync(`./data/${now.format("YYYY-MM-DD")}.html`, body);
+}
+
+/**
+ *  模拟同步请求
+ * 
+ * @param {object} options 请求参数
+ */
+function syncRequest(options) {
+    return new Promise(function (resolve, reject) {
+        request(options, function (error, response, body) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(body);
+            }
+        });
+    });
+}
+
+/**
+ *  是否已登录
+ * @param {html} $ 
+ */
+function isLogin($) {
+    // 已经登录会出现用户个人头像,否则不出现
+    var userAvatar = $("#react-root > div:nth-child(1) > div.J-header.c-nav-wrap.c-nav.com-2-nav.c-nav-air-sub > div > div.J-headerBottom.c-nav-bottom.responsive > div.J-headerBottomRight.c-nav-bm-right > div:nth-child(4) > a");
+
+    var loginFlag = userAvatar && userAvatar.attr("href");
+    if (loginFlag) {
+        console.log("已经登录: " + userAvatar.attr("href"));
+
+        return true;
+    } else {
+        console.log("尚未登录");
+
+        return false;
+    }
 }
 
 /**
@@ -134,21 +174,7 @@ async function parsePagenation() {
     return total;
 }
 
-/**
- *  同步请求
- * @param {object} options 
- */
-function syncRequest(options) {
-    return new Promise(function(resolve, reject) {
-        request(options, function(error, response, body) {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(body);
-            }
-        });
-    });
-}
+
 
 /**
  *  是否是最大页码
@@ -159,25 +185,7 @@ function isTotal(body) {
     return body.code == 0 && body.data.list.length == 0;
 }
 
-/**
- *  是否已登录
- * @param {html} $ 
- */
-function isLogin($) {
-    // 已经登录会出现用户个人头像,否则不出现
-    var userAvatar = $("#react-root > div:nth-child(1) > div.J-header.c-nav-wrap.c-nav.com-2-nav.c-nav-air-sub > div > div.J-headerBottom.c-nav-bottom.responsive > div.J-headerBottomRight.c-nav-bm-right > div:nth-child(4) > a");
 
-    var loginFlag = userAvatar && userAvatar.attr("href");
-    if (loginFlag) {
-        console.log("已经登录: " + userAvatar.attr("href"));
-
-        return true;
-    } else {
-        console.log("尚未登录");
-
-        return false;
-    }
-}
 
 /**
  *  解析首页
@@ -216,7 +224,7 @@ function parseCurrent(body) {
             result.commentCount += commentCount;
 
             // 当前页正在解析中
-            console.log(`当前页面解析中,一共${atricles.length}篇文章,正在解析第${i+1}篇,标题: ${title} 阅读量: ${readCount} 点赞数: ${recommendCount} 评论数: ${commentCount}`);
+            console.log(`当前页面解析中,一共${atricles.length}篇文章,正在解析第${i + 1}篇,标题: ${title} 阅读量: ${readCount} 点赞数: ${recommendCount} 评论数: ${commentCount}`);
         }
     }
 
@@ -262,6 +270,6 @@ function parseIndex2(body) {
         console.log("recommendCount", recommendCount);
 
         // 总体概况
-        console.log(`一共解析出${atricles.length}篇文章,正在解析第${i+1}篇,标题: ${titile} 阅读量: ${readCount} 点赞量: ${recommendCount}`);
+        console.log(`一共解析出${atricles.length}篇文章,正在解析第${i + 1}篇,标题: ${titile} 阅读量: ${readCount} 点赞量: ${recommendCount}`);
     }
 }
