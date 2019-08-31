38440b33bc76293ac8ca2600d985527062ecfadf
snowdreams1006 <snowdreams1006@163.com>
2019-09-01 00:48:46 +0800

    add csdn

--- count/csdn.js
+++ count/csdn.js
@@ -1,13 +1,49 @@
-//阅读量
+// 阅读数
 var readCount = 0;
-$.each($("#mainBox .article-list .article-item-box .info-box p:nth-child(3) > span > span"),function(idx,ele){
-     readCount += parseInt($(ele).text().trim());
+$.each($("#mainBox .article-list .article-item-box .info-box p:nth-child(3) > span > span"), function(idx, ele) {
+    readCount += parseInt($(ele).text().trim());
 });
-console.log("阅读量: " + readCount);
-
-//评论量
+// 评论数
 var commendCount = 0;
-$.each($("#mainBox .article-list .article-item-box .info-box p:nth-child(5) > span > span"),function(idx,ele){
-     commendCount += parseInt($(ele).text().trim());
+$.each($("#mainBox .article-list .article-item-box .info-box p:nth-child(5) > span > span"), function(idx, ele) {
+    commendCount += parseInt($(ele).text().trim());
 });
-console.log("评论量: " + commendCount);
+console.log("阅读数:" + readCount + ",评论数:" + commendCount);
+
+// https://blog.csdn.net/weixin_38171180
+var nickname = $("#uid").text();
+var originalArticle = $("#asideProfile > div.data-info.d-flex.item-tiling > dl:nth-child(1) > dd > a > span").text();
+var fans = $("#fan").text();
+var like = $("#asideProfile > div.data-info.d-flex.item-tiling > dl:nth-child(3) > dd > span").text();
+var comment = $("#asideProfile > div.data-info.d-flex.item-tiling > dl:nth-child(4) > dd > span").text();
+var level = $("#asideProfile > div.grade-box.clearfix > dl:nth-child(1) > dd > a").attr("title").replace(/[^0-9]/ig, "");
+var access = $("#asideProfile > div.grade-box.clearfix > dl:nth-child(2) > dd").text().trim();;
+var integral = $("#asideProfile > div.grade-box.clearfix > dl:nth-child(3) > dd").text().trim();;
+var rank = $("#asideProfile > div.grade-box.clearfix > dl:nth-child(4)").attr("title");
+var medal = "";
+$.each($("#asideProfile > div.badge-box.d-flex .icon-badge"), function(idx, ele) {
+    medal += $(ele).attr("title") + ","
+});
+medal = medal.substr(0, medal.length - 1);
+
+console.log("昵称:" + nickname + ",原创:" + originalArticle + ",粉丝:" + fans + ",喜欢:" + like + ",评论:" + comment + ",等级:" + level + ",访问:" + access + ",积分:" + integral + ",排名:" + rank + ",勋章:" + medal);
+
+// 分页汇总数据
+var countStatics = `阅读数:289,评论数:2
+阅读数:1387,评论数:0
+阅读数:1140,评论数:0
+阅读数:1001,评论数:0`;
+
+// 阅读,推荐,评论
+var readCountStatics = 0;
+var commendCountStatics = 0;
+$.each($(countStatics.split("\n")), function(idx, val) {
+    $.each($(val.split(",")), function(sIdx, sVal) {
+        if (sIdx == 0) {
+            readCountStatics += parseInt(sVal.replace(/[^0-9]/ig, ""));
+        } else {
+            commendCountStatics += parseInt(sVal.replace(/[^0-9]/ig, ""));
+        }
+    });
+});
+console.log("阅读数:" + readCountStatics + ",评论数:" + commendCountStatics);
--- count/imooc.js
+++ count/imooc.js
@@ -1,26 +1,50 @@
-// 作者基本信息
-var nickname = $("#main > div.bg-author.user-head-info > div > div.user-info-right > h3 > span").text();
-var learnTime = $("#main > div.bg-author.user-head-info > div > div.study-info.clearfix > div:nth-child(1) > div > em").text();
-var learnExperience = $("#main > div.bg-author.user-head-info > div > div.study-info.clearfix > div:nth-child(2) > em").text();
-var learnIntegral = $("#main > div.bg-author.user-head-info > div > div.study-info.clearfix > div:nth-child(3) > em").text();
-var learnFollows = $("#main > div.bg-author.user-head-info > div > div.study-info.clearfix > div:nth-child(4) > a > em").text();
-var learnFans = $("#main > div.bg-author.user-head-info > div > div.study-info.clearfix > div:nth-child(5) > a > em").text();
-var articleCount = $("#articleMain > ul > div > div > div > div > a:nth-child(1)").text().replace(/[^0-9]/ig, "");
-console.log("昵称: " + nickname + " 学习时长: " + learnTime + " 经验: " + learnExperience + " 积分: " + learnIntegral + " 关注: " + learnFollows + " 粉丝: " + learnFans + " 全部文章: " + articleCount);
-
-// 阅读量
+// 阅读
 var readCount = 0;
 $.each($("#articlesList div:nth-child(1) > em").text().trim().split(" "), function(idx, ele) {
     readCount += parseInt(ele.substr(0, ele.lastIndexOf("浏览")));
 });
-// 推荐量
+// 推荐
 var recommendCount = 0;
 $.each($("#articlesList div:nth-child(2) > em").text().trim().split(" "), function(idx, ele) {
     recommendCount += parseInt(ele.substr(0, ele.lastIndexOf("推荐")));
 });
-// 评论量
+// 评论
 var commendCount = 0;
 $.each($("#articlesList div:nth-child(3) > em").text().trim().split(" "), function(idx, ele) {
     commendCount += parseInt(ele.substr(0, ele.lastIndexOf("评论")));
 });
-console.log("阅读量: " + readCount + " 推荐量: " + recommendCount + " 评论量: " + commendCount);
+console.log("阅读:" + readCount + ",推荐:" + recommendCount + ",评论:" + commendCount);
+
+// https://www.imooc.com/u/5224488/articles
+var nickname = $("#main > div.bg-author.user-head-info > div > div.user-info-right > h3 > span").text();
+var learnTime = $("#main > div.bg-author.user-head-info > div > div.study-info.clearfix > div:nth-child(1) > div > em").text();
+var learnExperience = $("#main > div.bg-author.user-head-info > div > div.study-info.clearfix > div:nth-child(2) > em").text();
+var learnIntegral = $("#main > div.bg-author.user-head-info > div > div.study-info.clearfix > div:nth-child(3) > em").text();
+var follows = $("#main > div.bg-author.user-head-info > div > div.study-info.clearfix > div:nth-child(4) > a > em").text();
+var fans = $("#main > div.bg-author.user-head-info > div > div.study-info.clearfix > div:nth-child(5) > a > em").text();
+var articleCount = $("#articleMain > ul > div > div > div > div > a:nth-child(1)").text().replace(/[^0-9]/ig, "");
+console.log("昵称:" + nickname + ",学习时长:" + learnTime + ",经验:" + learnExperience + ",积分:" + learnIntegral + ",关注:" + follows + ",粉丝:" + fans + ",全部文章:" + articleCount);
+
+// 分页汇总数据
+var countStatics = `阅读:6167,推荐:25,评论:1
+阅读:11949,推荐:34,评论:1
+阅读:8274,推荐:24,评论:1
+阅读:3480,推荐:16,评论:1`;
+
+// 阅读,推荐,评论
+var readCountStatics = 0;
+var recommendCountStatics = 0;
+var commendCountStatics = 0;
+$.each($(countStatics.split("\n")), function(idx, val) {
+    $.each($(val.split(",")), function(sIdx, sVal) {
+        if (sIdx == 0) {
+            readCountStatics += parseInt(sVal.replace(/[^0-9]/ig, ""));
+        } else if (sIdx == 1) {
+            recommendCountStatics += parseInt(sVal.replace(/[^0-9]/ig, ""));
+        } else {
+            commendCountStatics += parseInt(sVal.replace(/[^0-9]/ig, ""));
+        }
+    });
+});
+console.log("阅读:" + readCountStatics + ",推荐:" + recommendCountStatics + ",评论:" + commendCountStatics);
+
